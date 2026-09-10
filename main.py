1a8b78552f5293634a4244a8366e218a94e67ea7
aoki098 <minoru.aoki00@gmail.com>
2026-09-10 15:18:40 +0900

    不必要なファイルの除去

--- main.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("template-streamlit")
-st.info("Select a code sample from the sidebar to run it")
--- template_streamlit/agents/receipt_processor/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-領収書OCRエージェントモジュール
-ref. https://github.com/mahm/softwaredesign-llm-application/tree/main/22
-"""
